6c153b817a8a98ed752176b8d5572d3ec0ce94bd
unknown <bbrown1867@gmail.com>
2015-08-31 09:35:38 -0400

    Major syntax mods to pipeline.

--- hw/vid_io_demo/verilog/pipeline_wrapper.v
+++ hw/vid_io_demo/verilog/pipeline_wrapper.v
@@ -31,53 +31,58 @@ module pipeline_wrapper
 
 		//////// SDRAM //////////
 		output wire [12:0] 	DRAM_ADDR,
-		output wire [ 1:0] 	DRAM_BA,
+		output wire [1:0] 	DRAM_BA,
 		output wire 		DRAM_CAS_N,
 		output wire 		DRAM_CKE,
 		output wire 		DRAM_CLK,
 		output wire 		DRAM_CS_N,
 		inout  wire [31:0]	DRAM_DQ,
-		output wire [ 3:0] 	DRAM_DQM,
+		output wire [3:0] 	DRAM_DQM,
 		output wire 		DRAM_RAS_N,
 		output wire 		DRAM_WE_N
 );
 
-//	For ITU-R 656 Decoder
-wire	[15:0]	YCbCr;
-wire	[9:0]	TV_X;
-wire			TV_DVAL;
+// Cascade Resets
+wire			true_reset;
+wire			true_reset0_n;
+wire			true_reset1_n;
+wire			true_reset2_n;
 
-//	For VGA Controller
+// ITU-R 656 Decoder
+wire	[15:0]	YCbCr;
+wire			YCbCr_valid;
+wire	[9:0]	decoder_x;
+
+// Down Sample
+wire	[3:0]	Remain;
+wire	[9:0]	Quotient;
+
+// For VGA Controller
 wire	[9:0]	mRed;
 wire	[9:0]	mGreen;
 wire	[9:0]	mBlue;
-wire	[10:0]	VGA_X;
-wire	[10:0]	VGA_Y;
+wire	[10:0]	vga_x;
+wire	[10:0]	vga_y;
 wire			VGA_Read;	//	VGA data request
 wire			m1VGA_Read;	//	Read odd field
 wire			m2VGA_Read;	//	Read even field
 
-//	For YUV 4:2:2 to YUV 4:4:4
+// For YUV 4:2:2 to YUV 4:4:4
 wire	[7:0]	mY;
 wire	[7:0]	mCb;
 wire	[7:0]	mCr;
 
-//	For field select
+//VGA Controller
+wire 	[9:0] 	vga_r10;
+wire 	[9:0] 	vga_g10;
+wire 	[9:0] 	vga_b10;
+
+// For field select
 wire	[15:0]	mYCbCr;
 wire	[15:0]	mYCbCr_d;
 wire	[15:0]	m1YCbCr;
 wire	[15:0]	m2YCbCr;
 wire	[15:0]	m3YCbCr;
-
-//	For Delay Timer
-wire			TD_Stable;
-wire			DLY0;
-wire			DLY1;
-wire			DLY2;
-
-//	For Down Sample
-wire	[3:0]	Remain;
-wire	[9:0]	Quotient;
 
 wire			mDVAL;
 
@@ -88,9 +93,14 @@ wire	[7:0]	Tmp3,Tmp4;
 
 assign	TD_RESET_N	=	1'b1;
 
-assign	m1VGA_Read	=	VGA_Y[0]		?	1'b0		:	VGA_Read	;
-assign	m2VGA_Read	=	VGA_Y[0]		?	VGA_Read	:	1'b0		;
-assign	mYCbCr_d	=	!VGA_Y[0]		?	m1YCbCr		:
+//VGA Controller module is 10-bit but DAC only does 8-bit
+assign VGA_R = vga_r10[9:2];
+assign VGA_G = vga_g10[9:2];
+assign VGA_B = vga_b10[9:2];
+
+assign	m1VGA_Read	=	vga_y[0]		?	1'b0		:	VGA_Read	;
+assign	m2VGA_Read	=	vga_y[0]		?	VGA_Read	:	1'b0		;
+assign	mYCbCr_d	=	~vga_y[0]		?	m1YCbCr		:
 											      m2YCbCr		;
 assign	mYCbCr		=	m5YCbCr;
 
@@ -101,147 +111,183 @@ assign	Tmp4	=	Tmp2[8:2]+m3YCbCr[15:9];
 assign	m5YCbCr	=	{Tmp4,Tmp3};
 							
 //	TV Decoder Stable Check
-td_detect			u2	(	.oTD_Stable(TD_Stable),
-							.iTD_VS(TD_VS),
-							.iTD_HS(TD_HS),
-							.iRST_N(aresetn)	);
+td_detect u2	
+(	
+	.iRST_N			(aresetn),
+	.iTD_VS			(TD_VS),
+	.iTD_HS			(TD_HS),
 
-//	Reset Delay Timer
-Reset_Delay			u3	(	.iCLK(clk),
-							.iRST(TD_Stable),
-							.oRST_0(DLY0),
-							.oRST_1(DLY1),
-							.oRST_2(DLY2));
+	.oTD_Stable 	(true_reset)
+);
+
+//	Reset Delay
+Reset_Delay	u3	
+(	
+	.iCLK 			(clk),
+	.iRST 			(true_reset),
+
+	.oRST_0 		(true_reset0_n),
+	.oRST_1			(true_reset1_n),
+	.oRST_2			(true_reset2_n)
+);
 
 //	ITU-R 656 to YUV 4:2:2
-ITU_656_Decoder		u4	(	//	TV Decoder Input
-							.iTD_DATA(TD_DATA),
-							//	Position Output
-							.oTV_X(TV_X),
-							//	YUV 4:2:2 Output
-							.oYCbCr(YCbCr),
-							.oDVAL(TV_DVAL),
-							//	Control Signals
-							.iSwap_CbCr(Quotient[0]),
-							.iSkip(Remain==4'h0),
-							.iRST_N(DLY1),
-							.iCLK_27(TD_CLK27)	);
+ITU_656_Decoder	u4	
+(	
+	.iCLK_27 		(TD_CLK27),
+	.iRST_N 		(true_reset1_n),
+	
+	.iTD_DATA 		(TD_DATA),
+	.iSwap_CbCr 	(Quotient[0]),
+	.iSkip 			(Remain==4'h0),
 
-//	For Down Sample 720 to 640
-DIV 				u5	(	.aclr(!DLY0),	
-							.clock(TD_CLK27),
-							.denom(4'h9),
-							.numer(TV_X),
-							.quotient(Quotient),
-							.remain(Remain));
+	.oTV_X 			(decoder_x),
+	.oYCbCr 		(YCbCr),
+	.oDVAL 			(YCbCr_valid)
+);
 
-//	SDRAM frame buffer
-Sdram_Control_4Port	u6	(	//	HOST Side
-						    .REF_CLK(TD_CLK27),
-						    .RESET_N(DLY0),
-							//	FIFO Write Side 1
-						    .WR1_DATA(YCbCr),
-							.WR1(TV_DVAL),
-							.WR1_FULL(WR1_FULL),
-							.WR1_ADDR(0),
-							.WR1_MAX_ADDR(640*507),		//	525-18							
-							.WR1_LENGTH(9'h80),
-							.WR1_LOAD(!DLY0),
-							.WR1_CLK(TD_CLK27),
-							//	FIFO Read Side 1
-						    .RD1_DATA(m1YCbCr),
-				        	.RD1(m1VGA_Read),
-				        	.RD1_ADDR(640*13),			//	Read odd field and bypess blanking
-							.RD1_MAX_ADDR(640*253),							
-							.RD1_LENGTH(9'h80),
-				        	.RD1_LOAD(!DLY0),
-							.RD1_CLK(TD_CLK27),
-							//	FIFO Read Side 2
-						    .RD2_DATA(m2YCbCr),
-				        	.RD2(m2VGA_Read),
-				        	.RD2_ADDR(640*267),			//	Read even field and bypess blanking
-							.RD2_MAX_ADDR(640*507),							
-							.RD2_LENGTH(9'h80),
-				        	.RD2_LOAD(!DLY0),
-							.RD2_CLK(TD_CLK27),
-							//	SDRAM Side
-						    .SA(DRAM_ADDR),
-						    .BA(DRAM_BA),
-						    .CS_N(DRAM_CS_N),
-						    .CKE(DRAM_CKE),
-						    .RAS_N(DRAM_RAS_N),
-				            .CAS_N(DRAM_CAS_N),
-				            .WE_N(DRAM_WE_N),
-						    .DQ(DRAM_DQ),
-				            .DQM({DRAM_DQM[1],DRAM_DQM[0]}),
-							.SDR_CLK(DRAM_CLK)	);
+//	Divide Megafuncion (Used to Down Sample)
+DIV u5	
+(	
+	.clock 			(TD_CLK27),
+	.aclr 			(~true_reset0_n),	
+	
+	.numer 			(decoder_x),
+	.denom 			(4'h9),			// 720 - 640 = 80, 720/80 = 9. Skip a sample every 9 pixels.
+
+	.quotient 		(Quotient),
+	.remain 		(Remain)
+);
+
+//	SDRAM Frame Buffer
+Sdram_Control_4Port	u6	
+(
+    .REF_CLK 		(TD_CLK27),
+    .RESET_N 		(true_reset0_n),
+
+	//	FIFO Write Side 1
+	.WR1_DATA 		(YCbCr),
+	.WR1 			(YCbCr_valid), 		// Write Enable
+	// .WR1_FULL 		(WR1_FULL),			
+	.WR1_ADDR 		(0),				//TODO: What is this number?
+	.WR1_MAX_ADDR 	(640*507),			//TODO: What is this number?						
+	.WR1_LENGTH 	(9'h80), 			//TODO: What is this number?
+	.WR1_LOAD 		(~true_reset0_n), 	// Clears FIFO
+	.WR1_CLK 		(TD_CLK27),
+
+	 // FIFO Read Side 1 (Odd Field, Bypass Blanking)
+    .RD1_DATA 		(m1YCbCr),
+	.RD1 			(m1VGA_Read), 		// Read Enable
+	.RD1_ADDR 		(640*13),			//TODO: What is this number?
+	.RD1_MAX_ADDR 	(640*253),			//TODO: What is this number?				
+	.RD1_LENGTH 	(9'h80),  			//TODO: What is this number?
+	.RD1_LOAD 		(~true_reset0_n),   // Clears FIFO
+	.RD1_CLK 		(TD_CLK27),
+
+	// FIFO Read Side 2 (Even Field, Bypass Blanking)
+    .RD2_DATA 		(m2YCbCr),
+	.RD2 			(m2VGA_Read),		// Read Enable
+	.RD2_ADDR 		(640*267),			//TODO: What is this number?
+	.RD2_MAX_ADDR 	(640*507),		 	//TODO: What is this number?				
+	.RD2_LENGTH 	(9'h80),            //TODO: What is this number?
+	.RD2_LOAD 		(!true_reset0_n),   // Clears FIFO
+	.RD2_CLK  		(TD_CLK27),
+
+	// SDRAM
+    .SA 			(DRAM_ADDR),
+    .BA 			(DRAM_BA),
+    .CS_N 			(DRAM_CS_N),
+    .CKE 			(DRAM_CKE),
+    .RAS_N 			(DRAM_RAS_N),
+    .CAS_N  		(DRAM_CAS_N),
+    .WE_N 			(DRAM_WE_N),
+    .DQ 			(DRAM_DQ),
+    .DQM 			({DRAM_DQM[1], DRAM_DQM[0]}),
+	.SDR_CLK 		(DRAM_CLK)	
+);
 
 //	YUV 4:2:2 to YUV 4:4:4
-YUV422_to_444		u7	(	//	YUV 4:2:2 Input
-							.iYCbCr(mYCbCr),
-							//	YUV	4:4:4 Output
-							.oY(mY),
-							.oCb(mCb),
-							.oCr(mCr),
-							//	Control Signals
-							.iX(VGA_X-160),
-							.iCLK(TD_CLK27),
-							.iRST_N(DLY0));
+YUV422_to_444 u7 
+(	
+	.iCLK 			(TD_CLK27),
+	.iRST_N 		(true_reset0_n),
+
+	.iX 			(vga_x-160),				// postion_x - H_BLANK
+	.iYCbCr 		(mYCbCr),
+	
+	.oY 			(mY),
+	.oCb 			(mCb),
+	.oCr  			(mCr)
+);
 
 //	YCbCr 8-bit to RGB-10 bit 
-YCbCr2RGB 			u8	(	//	Output Side
-							.Red(mRed),
-							.Green(mGreen),
-							.Blue(mBlue),
-							.oDVAL(mDVAL),
-							//	Input Side
-							.iY(mY),
-							.iCb(mCb),
-							.iCr(mCr),
-							.iDVAL(VGA_Read),
-							//	Control Signal
-							.iRESET(!DLY2),
-							.iCLK(TD_CLK27));
+YCbCr2RGB u8
+(
+	.iCLK 			(TD_CLK27),
+	.iRESET 		(~true_reset2_n),
+
+	.iDVAL 			(VGA_Read),
+	.iY 			(mY),
+	.iCb  			(mCb),
+	.iCr 			(mCr),
+
+	.Red 			(mRed),
+	.Green 			(mGreen),
+	.Blue 			(mBlue),
+	.oDVAL 			(mDVAL),
+);
 
 //	VGA Controller
-wire [9:0] vga_r10;
-wire [9:0] vga_g10;
-wire [9:0] vga_b10;
-assign VGA_R = vga_r10[9:2];
-assign VGA_G = vga_g10[9:2];
-assign VGA_B = vga_b10[9:2];
+VGA_Ctrl u9	
+(
+	.iCLK 			(TD_CLK27),
+	.iRST_N 		(true_reset2_n),		
 
-VGA_Ctrl			u9	(	//	Host Side
-							.iRed(mRed),
-							.iGreen(mGreen),
-							.iBlue(mBlue),
-							.oCurrent_X(VGA_X),
-							.oCurrent_Y(VGA_Y),
-							.oRequest(VGA_Read),
-							//	VGA Side
-							.oVGA_R(vga_r10 ),
-							.oVGA_G(vga_g10 ),
-							.oVGA_B(vga_b10 ),
-							.oVGA_HS(VGA_HS),
-							.oVGA_VS(VGA_VS),
-							.oVGA_SYNC(VGA_SYNC_N),
-							.oVGA_BLANK(VGA_BLANK_N),
-							.oVGA_CLOCK(VGA_CLK),
-							//	Control Signal
-							.iCLK(TD_CLK27),
-							.iRST_N(DLY2)	);
+	//Input Data
+	.iRed 			(mRed),
+	.iGreen 		(mGreen),
+	.iBlue 			(mBlue),
 
-//	Line buffer, delay one line
-Line_Buffer u10	(	.aclr(!DLY0),
-					.clken(VGA_Read),
-					.clock(TD_CLK27),
-					.shiftin(mYCbCr_d),
-					.shiftout(m3YCbCr));
+	//VGA Position
+	.oCurrent_X 	(vga_x),
+	.oCurrent_Y 	(vga_y),
 
-Line_Buffer u11	(	.aclr(!DLY0),
-					.clken(VGA_Read),
-					.clock(TD_CLK27),
-					.shiftin(m3YCbCr),
-					.shiftout(m4YCbCr));
+	//VGA Ready
+	.oRequest 		(VGA_Read),
+
+	//Generate VGA Signals
+	.oVGA_R 		(vga_r10),
+	.oVGA_G 		(vga_g10),
+	.oVGA_B 		(vga_b10),
+	.oVGA_HS 		(VGA_HS),
+	.oVGA_VS 		(VGA_VS),
+	.oVGA_SYNC 		(VGA_SYNC_N),
+	.oVGA_BLANK 	(VGA_BLANK_N),
+	.oVGA_CLOCK 	(VGA_CLK)
+);
+
+//	Shift Register Megafunction
+Line_Buffer u10	
+(	
+	.clock 			(TD_CLK27),
+	.aclr 			(~true_reset0_n),
+	.clken 			(VGA_Read),
+	
+	.shiftin  		(mYCbCr_d),
+
+	.shiftout 		(m3YCbCr)
+);
+
+//	Shift Register Megafunction
+Line_Buffer u11	
+(	
+	.clock 			(TD_CLK27),
+	.aclr 			(~true_reset0_n),
+	.clken 			(VGA_Read),
+	
+	.shiftin 		(m3YCbCr),
+
+	.shiftout 		(m4YCbCr)
+);
 
 endmodule
