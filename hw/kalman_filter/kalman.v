b7e5e83e48d3fbb69e2904ce0efabc4e32bf42a9
unknown <bbrown1867@gmail.com>
2015-10-27 18:37:08 +0100

    Basic matrix math works with fixed pt lib

--- hw/kalman_filter/kalman.v
+++ hw/kalman_filter/kalman.v
@@ -41,28 +41,25 @@ module kalman #(
 ////////////////////////////////////// PARAMETERS //////////////////////////////////////////////
 
 // Finite State Machine
-localparam FSM_WIDTH 	 = 4;
+localparam FSM_WIDTH 	 = 3;
 localparam FSM_INIT		 = 0;
 localparam FSM_IDLE      = 1;
 localparam FSM_PREDICT_1 = 2;
 localparam FSM_PREDICT_2 = 3;
-localparam FSM_NORMAL_1  = 4;
-localparam FSM_INTERM_1  = 5;
-localparam FSM_INTERM_2  = 6;
-localparam FSM_INTERM_3  = 7;
-localparam FSM_UPDATE    = 8;
+localparam FSM_UPDATE    = 4;
 
 // Architecture
-localparam ARCH_W 		= 31;
+localparam ARCH_W 		= 32;
+localparam ARCH_F 		= 15;
 
 // Kalman State Space
 localparam NUM_STATES 	= 4;
 localparam NUM_MEASUR 	= 2;
 
-// Fixed Point Values
-localparam FRAC   	 	= 6;
-localparam ONE_FI 		= 1 << FRAC;
-localparam T_STEP_FI 	= 1; 	// round(0.0155 << FRAC)
+// Fixed Point Values - sign | integer | fraction
+localparam ONE_FI 		= ('b0 << ARCH_W) | ('b1 << (ARCH_W-ARCH_F-2)) | 'b0;
+localparam TSTEP_FI 	= ('b0 << ARCH_W) | ('b0 << (ARCH_W-ARCH_F-2)) | 'b000000111111100;
+localparam RDIAG_FI     = ('b0 << ARCH_W) | ('d1000 << (ARCH_W-ARCH_F-2)) | 'b0; 
 
 /////////////////////////////// INTERNAL SIGNALS & VARIABLES ///////////////////////////////////
 
@@ -73,25 +70,24 @@ wire 					fsm_clear_all;
 wire 					fsm_clear_tmp;
 
 // Static Matrices
-wire 		[(ARCH_W-1):0]		x_init	   [0:NUM_STATES-1];
-wire 		[(ARCH_W-1):0]		p_init	   [0:NUM_STATES-1][0:NUM_STATES-1];
-wire 		[(ARCH_W-1):0] 		f_mat 	   [0:NUM_STATES-1][0:NUM_STATES-1];
-wire 		[(ARCH_W-1):0]		q_mat 	   [0:NUM_STATES-1][0:NUM_STATES-1];
-wire 		[(ARCH_W-1):0] 		r_mat 	   [0:NUM_MEASUR-1][0:NUM_MEASUR-1];
-wire signed [(ARCH_W-1):0]		p_helper   [0:NUM_STATES-1][0:NUM_STATES-1];
-wire signed [(ARCH_W-1):0]		s_det;
+wire [(ARCH_W-1):0]		x_init	    [0:NUM_STATES-1];
+wire [(ARCH_W-1):0]		p_init	    [0:NUM_STATES-1][0:NUM_STATES-1];
+wire [(ARCH_W-1):0] 	f_mat 	    [0:NUM_STATES-1][0:NUM_STATES-1];
+wire [(ARCH_W-1):0]		q_mat 	    [0:NUM_STATES-1][0:NUM_STATES-1];
+wire [(ARCH_W-1):0] 	r_mat 	    [0:NUM_MEASUR-1][0:NUM_MEASUR-1];
 
-// Dynamic Matrices and Vectors
-reg  		[(ARCH_W-1):0]		x_curr 	   [0:NUM_STATES-1];
-reg  		[(ARCH_W-1):0]		x_next 	   [0:NUM_STATES-1];
-reg  		[(ARCH_W-1):0]		p_curr 	   [0:NUM_STATES-1][0:NUM_STATES-1];
-reg  		[(ARCH_W-1):0]		p_next_tmp [0:NUM_STATES-1][0:NUM_STATES-1];
-reg  		[(ARCH_W-1):0]		p_next 	   [0:NUM_STATES-1][0:NUM_STATES-1];
-reg  signed [(ARCH_W-1):0]		z_vec	   [0:NUM_MEASUR-1];
-reg  signed [(ARCH_W-1):0] 	y_vec	   [0:NUM_MEASUR-1];
-reg  signed [(ARCH_W-1):0]		s_mat	   [0:NUM_MEASUR-1][0:NUM_MEASUR-1];
-reg  signed [(ARCH_W-1):0]		s_mat_inv  [0:NUM_MEASUR-1][0:NUM_MEASUR-1];
-reg  signed [(ARCH_W-1):0]		k_mat 	   [0:NUM_STATES-1][0:NUM_MEASUR-1];
+// State Vector
+reg  [(ARCH_W-1):0]		x_curr		[0:NUM_STATES-1];
+reg  [(ARCH_W-1):0]		x_next 	    [0:NUM_STATES-1];
+wire [(ARCH_W-1):0]		x_next_mult [0:NUM_STATES-1][0:NUM_STATES-1];
+wire [(ARCH_W-1):0]		x_next_sum1 [0:NUM_STATES-1];
+wire [(ARCH_W-1):0]		x_next_sum2 [0:NUM_STATES-1];
+wire [(ARCH_W-1):0]		x_next_sum3 [0:NUM_STATES-1];
+
+// Covariance Matrix
+reg  [(ARCH_W-1):0]		p_curr		[0:NUM_STATES-1][0:NUM_STATES-1];
+reg  [(ARCH_W-1):0]		p_next 	    [0:NUM_STATES-1][0:NUM_STATES-1];
+reg  [(ARCH_W-1):0]		p_next_tmp  [0:NUM_STATES-1][0:NUM_STATES-1];
 
 // Loops that get rolled out on compile time
 genvar i, j;
@@ -110,11 +106,7 @@ always @* begin
 		FSM_INIT 	  : fsm_next = FSM_IDLE;
 		FSM_IDLE 	  : fsm_next = valid ? FSM_PREDICT_1 : FSM_IDLE;
 		FSM_PREDICT_1 : fsm_next = FSM_PREDICT_2;
-		FSM_PREDICT_2 : fsm_next = FSM_NORMAL_1;
-		FSM_NORMAL_1  : fsm_next = FSM_INTERM_1;
-		FSM_INTERM_1  : fsm_next = FSM_INTERM_2;
-		FSM_INTERM_2  : fsm_next = FSM_INTERM_3;
-		FSM_INTERM_3  : fsm_next = FSM_UPDATE;
+		FSM_PREDICT_2 : fsm_next = FSM_UPDATE;
 		FSM_UPDATE    : fsm_next = FSM_IDLE;
 		default       : fsm_next = FSM_INIT;
 	endcase 
@@ -127,28 +119,26 @@ assign fsm_clear_tmp = (fsm_curr == FSM_INIT) | (fsm_curr == FSM_IDLE);
 
 ///////////////////////////////// STATIC MATRICES AND VECTORS //////////////////////////////////
 
-// All of these have F = 0, except f_mat which has F = FRAC and q_mat and has F = 2*FRAC
-
 // Set x_init to zeros, and p_init and q_mat to identity. Set f_mat to identity with t_step.
 generate
 	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_vec_init
-		assign x_init[i] = 0;
+		assign x_init[i] = ONE_FI;
 		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_mat_init
 			if (i == j) 
 				begin
-					assign p_init[i][j] = 'd1;
-					assign q_mat[i][j] = ('d1 << 2*FRAC);
+					assign p_init[i][j] = ONE_FI;
+					assign q_mat[i][j] = ONE_FI;
 					assign f_mat[i][j] = ONE_FI;
 				end
 			else if ((i == 0 & j == 2) | (i == 1 & j == 3))
 				begin
 					assign p_init[i][j] = 'd0;
 					assign q_mat[i][j] = 'd0;
-					assign f_mat[i][j] = T_STEP_FI;
+					assign f_mat[i][j] = TSTEP_FI;
 				end
 			else 
 				begin
-					assign p_init[i][j] = 'd0;
+					assign p_init[i][j] ='d0;
 					assign q_mat[i][j] = 'd0;
 					assign f_mat[i][j] = 'd0;
 				end
@@ -160,155 +150,66 @@ endgenerate
 generate 
 	for (i = 0; i < NUM_MEASUR; i = i + 1) begin: gen_r_mat_rows
 		for (j = 0; j < NUM_MEASUR; j = j + 1) begin: gen_r_mat_cols
-			if (i == j)	assign r_mat[i][j] = 'd1000;
+			if (i == j)	assign r_mat[i][j] = RDIAG_FI;
 			else		assign r_mat[i][j] = 'd0;
-		end
-	end
-endgenerate
-
-// Init p_helper : This matrix just rearranges K into a (4,4) subtracted from the identity. Used in update state.
-generate
-	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_helper_rows
-		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_helper_cols
-			if ((i == j) & i < 2)	assign p_helper[i][j] = 'd1 - k_mat[i][j];
-			else if (i == j)		assign p_helper[i][j] = 'd1;
-			else if (j < 2)			assign p_helper[i][j] = k_mat[i][j];
-			else					assign p_helper[i][j] = 'd0;
 		end
 	end
 endgenerate
 
 //////////////////////////////// DYNAMIC MATRICES AND VECTORS //////////////////////////////////
 
-// Measurement Value - 2x1 vector latched when valid data arrives
-always @(posedge clk or negedge aresetn) begin
-	if (~aresetn) 
-		begin
-			z_vec[0] <= 'd0;
-			z_vec[1] <= 'd0;
-		end
- 	else if (ready & valid)	
- 		begin
- 			z_vec[0] <= z_x;
- 			z_vec[1] <= z_y;
- 		end
- 	else 
- 		begin
- 			z_vec[0] <= z_vec[0];
- 			z_vec[1] <= z_vec[1];
- 		end
-end
-
 // Next X Value - 4x4 matrix times a 4x1 vector
 generate
 	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_x_next
 		always @(posedge clk) begin
 			if (fsm_clear_tmp) 					x_next[i] <= 'd0;
-			// Compute, F = FRAC
-			else if (fsm_curr == FSM_PREDICT_1)	x_next[i] <= f_mat[i][0]*x_curr[0] + f_mat[i][1]*x_curr[1] + f_mat[i][2]*x_curr[2] + f_mat[i][3]*x_curr[3];
-			// Normalize, F = 0
-			else if (fsm_curr == FSM_NORMAL_1)  x_next[i] <= x_next[i] >> FRAC;
+			else if (fsm_curr == FSM_PREDICT_1)	x_next[i] <= x_next_sum3[i];
 			else								x_next[i] <= x_next[i];
 		end
 	end
 endgenerate
 
-// Temp P Value - 4x4 matrix times a 4x4 matrix (F = 6)
 generate
-	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_p_temp_rows
-		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_p_temp_cols
-			always @(posedge clk) begin
-				if (fsm_clear_tmp)		 			p_next_tmp[i][j] <= 'd0;
-				// Compute, F = FRAC
-				else if (fsm_curr == FSM_PREDICT_1) p_next_tmp[i][j] <= f_mat[i][0]*p_curr[0][j] + f_mat[i][1]*p_curr[1][j] + f_mat[i][2]*p_curr[2][j] + f_mat[i][3]*p_curr[3][j];
-				else								p_next_tmp[i][j] <= p_next_tmp[i][j];
-			end
+	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_x_mult_modules_row
+		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_x_mult_modules_col
+			qmult #(
+				.Q 				(ARCH_F),
+				.N 				(ARCH_W)
+			) mult_x_next (
+		 		.i_multiplicand (f_mat[i][j]),
+		 		.i_multiplier	(x_curr[j]),
+		 		.o_result		(x_next_mult[i][j])
+			);
 		end
 	end
 endgenerate
 
-// Next P Value - 4x4 matrix times a 4x4 transposed matrix, plus a 4x4 matrix
 generate
-	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_p_next_rows
-		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_p_next_cols
-			always @(posedge clk) begin
-				if (fsm_clear_tmp)		 			p_next[i][j] <= 'd0;
-				// Compute, F = 2*FRAC
-				else if (fsm_curr == FSM_PREDICT_2)	p_next[i][j] <= (p_next_tmp[i][0]*f_mat[j][0] + p_next_tmp[i][1]*f_mat[j][1] + p_next_tmp[i][2]*f_mat[j][2] + p_next_tmp[i][3]*f_mat[j][3]) + q_mat[i][j];
-				// Normalize, F = 0
-				else if (fsm_curr == FSM_NORMAL_1)  p_next[i][j] <= p_next[i][j] >> 2*FRAC;
-				else								p_next[i][j] <= p_next[i][j];
-			end
-		end
-	end
-endgenerate
-
-// Y Vector - 2x1 measurement vector (inputs) minus predicted position vector, the first two entries of the 4x1 vector x_next
-generate
-	for (i = 0; i < NUM_MEASUR; i = i + 1) begin: gen_y_vec
-		always @(posedge clk) begin
-			if (fsm_clear_tmp) 					y_vec[i] <= 'd0;
-			// Compute, F = 0
-			else if (fsm_curr == FSM_INTERM_1)	y_vec[i] <= z_vec[i] - x_next[i];
-			else 								y_vec[i] <= y_vec[i];
-		end	
-	end
-endgenerate
-
-// S Matrx - 2x2 matrix (top left corner of p_next) plus the R matrix
-generate
-	for (i = 0; i < NUM_MEASUR; i = i + 1) begin: gen_s_rows
-		for (j = 0; j < NUM_MEASUR; j = j + 1) begin: gen_s_cols
-			always @(posedge clk) begin
-				if (fsm_clear_tmp)					s_mat[i][j] <= 'd0;
-				// Compute, F = 0
-				else if (fsm_curr == FSM_INTERM_1)  s_mat[i][j] <= p_next[i][j] + r_mat[i][j];
-				else 								s_mat[i][j] <= s_mat[i][j];
-			end
-		end
-	end
-endgenerate
-
-// S Matrix Determinment - Since s_mat has F = 0, so does this.
-assign s_det = s_mat[0][0]*s_mat[1][1] - s_mat[0][1]*s_mat[1][0];
-
-// S Matrix Inverse - Swap places of entries in s_mat, modify signs where necessary, and divide by determinant
-always @(posedge clk) begin
-	if (fsm_clear_tmp)								 s_mat_inv[0][0] <= 'd0;
-	// Compute, F = 0 assuming that integer division is going on
-	else if (fsm_curr == FSM_INTERM_2 & s_det != 0)  s_mat_inv[0][0] <= s_mat[1][1] / s_det;
-	else 											 s_mat_inv[0][0] <= s_mat_inv[0][0];
-end
-always @(posedge clk) begin
-	if (fsm_clear_tmp)								 s_mat_inv[1][1] <= 'd0;
-	// Compute, F = 0 assuming that integer division is going on
-	else if (fsm_curr == FSM_INTERM_2 & s_det != 0)  s_mat_inv[1][1] <= s_mat[0][0] / s_det;
-	else 											 s_mat_inv[1][1] <= s_mat_inv[1][1];
-end
-always @(posedge clk) begin
-	if (fsm_clear_tmp)								 s_mat_inv[0][1] <= 'd0;
-	// Compute, F = 0 assuming that integer division is going on
-	else if (fsm_curr == FSM_INTERM_2 & s_det != 0)  s_mat_inv[0][1] <= -1*s_mat[0][1] / s_det;
-	else 											 s_mat_inv[0][1] <= s_mat_inv[0][1];
-end
-always @(posedge clk) begin
-	if (fsm_clear_tmp)								 s_mat_inv[1][0] <= 'd0;
-	// Compute, F = 0 assuming that integer division is going on
-	else if (fsm_curr == FSM_INTERM_2 & s_det != 0)  s_mat_inv[1][0] <= -1*s_mat[1][0] / s_det;
-	else 											 s_mat_inv[1][0] <= s_mat_inv[1][0];
-end
-
-// K Matrix - 4x2 matrix (left half of p_next) times 2x2 matrix (s_mat_inv)
-generate
-	for (i = 0; i < NUM_STATES; i = i + 1) begin: k_gen_rows
-		for (j = 0; j < NUM_MEASUR; j = j + 1) begin: k_gen_cols
-			always @(posedge clk) begin
-				if (fsm_clear_tmp)					k_mat[i][j] <= 'd0;
-				// Compute, F = 0
-				else if (fsm_curr == FSM_INTERM_3)  k_mat[i][j] <= p_next[i][0]*s_mat_inv[0][j] + p_next[i][1]*s_mat_inv[1][j];
-				else 								k_mat[i][j] <= k_mat[i][j];
-			end
-		end
+	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_x_add_modules
+		qadd #(
+			.Q 				(ARCH_F),
+			.N 				(ARCH_W)
+		) add_x_next1 (
+			.a 				(x_next_mult[i][0]),
+			.b 				(x_next_mult[i][1]),
+			.c 				(x_next_sum1[i])
+		);
+		qadd #(
+			.Q 				(ARCH_F),
+			.N 				(ARCH_W)
+		) add_x_next2 (
+			.a 				(x_next_mult[i][2]),
+			.b 				(x_next_mult[i][3]),
+			.c 				(x_next_sum2[i])
+		);
+		qadd #(
+			.Q 				(ARCH_F),
+			.N 				(ARCH_W)
+		) add_x_next3 (
+			.a 				(x_next_sum1[i]),
+			.b 				(x_next_sum2[i]),
+			.c 				(x_next_sum3[i])
+		);			
 	end
 endgenerate
 
@@ -317,13 +218,41 @@ generate
 	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_x_curr
 		always @(posedge clk) begin
 			if (fsm_clear_all)		 			x_curr[i] <= x_init[i];
-			// Compute, F = 0
-			else if (fsm_curr == FSM_UPDATE)	x_curr[i] <= x_next[i] + (k_mat[i][0]*y_vec[0] + k_mat[i][1]*y_vec[1]);
+			else if (fsm_curr == FSM_UPDATE)	x_curr[i] <= x_next[i];
 			else 								x_curr[i] <= x_curr[i];
 		end
 	end
 endgenerate
 
+// Temp P Value - 4x4 matrix times a 4x4 matrix
+generate
+	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_p_temp_rows
+		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_p_temp_cols
+			always @(posedge clk) begin
+				if (fsm_clear_tmp)		 			p_next_tmp[i][j] <= 'd0;
+				else if (fsm_curr == FSM_PREDICT_1) p_next_tmp[i][j] <= 'bx;
+				else								p_next_tmp[i][j] <= p_next_tmp[i][j];
+			end
+		end
+	end
+endgenerate
+
+// MATH
+
+// Next P Value - 4x4 matrix times a 4x4 transposed matrix, plus a 4x4 matrix
+generate
+	for (i = 0; i < NUM_STATES; i = i + 1) begin: gen_p_next_rows
+		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_p_next_cols
+			always @(posedge clk) begin
+				if (fsm_clear_tmp)		 			p_next[i][j] <= 'd0;
+				else if (fsm_curr == FSM_PREDICT_2)	p_next[i][j] <= 'bx;
+				else								p_next[i][j] <= p_next[i][j];
+			end
+		end
+	end
+endgenerate
+
+// MATH
 
 // Current P Value - 4x4 weird reconstruction of K matrix (done above in p_helper) times a 4x4 matrix
 generate
@@ -331,16 +260,18 @@ generate
 		for (j = 0; j < NUM_STATES; j = j + 1) begin: gen_p_curr_cols
 			always @(posedge clk) begin
 				if (fsm_clear_all)		 			p_curr[i][j] <= p_init[i][j];
-				// Compute, F = 0
-				else if (fsm_curr == FSM_UPDATE) 	p_curr[i][j] <= p_helper[i][0]*p_next[0][j] + p_helper[i][1]*p_next[1][j] + p_helper[i][2]*p_next[2][j] + p_helper[i][3]*p_next[3][j];
+				else if (fsm_curr == FSM_UPDATE) 	p_curr[i][j] <= 'bx;
 				else								p_curr[i][j] <= p_curr[i][j];
 			end
 		end
 	end
 endgenerate
 
-// Drive Outputs
-assign z_x_new = x_curr[0];
-assign z_y_new = x_curr[1];
+
+// Generate Outputs
+wire [(ARCH_W-1):0] strange_1 = x_curr[0];
+wire [(ARCH_W-1):0] strange_2 = x_curr[1];
+assign z_x_new = strange_1[(ARCH_F+DISP_WIDTH):ARCH_F];
+assign z_y_new = strange_2[(ARCH_F+DISP_WIDTH):ARCH_F];
 
 endmodule
--- hw/kalman_filter/sim/signals.do
+++ hw/kalman_filter/sim/signals.do
@@ -5,21 +5,9 @@ add wave -logic clk
 add wave -logic rst_n
 add wave -logic valid
 add wave -logic ready
-add wave -logic myKalman/fsm_clear_all
-add wave -logic myKalman/fsm_clear_tmp
-add wave -logic -radix unsigned x_pos
-add wave -logic -radix unsigned y_pos
-add wave -logic -radix unsigned x_res
-add wave -logic -radix unsigned y_res
 add wave -logic -radix unsigned myKalman/fsm_curr
 add wave -logic -radix unsigned myKalman/x_next
 add wave -logic -radix unsigned myKalman/x_curr
-add wave -logic -radix decimal myKalman/p_next
-add wave -logic -radix decimal myKalman/p_curr
-add wave -logic -radix decimal myKalman/y_vec
-add wave -logic -radix decimal myKalman/s_mat
-add wave -logic -radix decimal myKalman/s_mat_inv
-add wave -logic -radix decimal myKalman/s_det
 add wave -logic -radix unsigned myKalman/z_x_new
 add wave -logic -radix unsigned myKalman/z_y_new
 
